c824e776ef1288f29828b187e2e9765ff51fdc1b
unknown <d4n.kh4n@gmail.com>
2024-10-11 01:20:48 +0500

    logic has been added

--- src/components/Reusable/TextInput/index.jsx
+++ src/components/Reusable/TextInput/index.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react';
+import InfoTooltip from '../InfoTooltip';
 
-const TextInput = () => {
+const TextInput = ({ question, onChange }) => {
+  const [text, setText] = useState('');
+
+  const handleChange = (e) => {
+    const value = e.target.value;
+    if (value.length <= 250) {
+      setText(value);
+      onChange(question.id, value);
+    }
+  };
+
   return (
-    <div>TextInput</div>
-  )
-}
+    <div className="question-wrapper">
+      <label>{question.title}</label>
+      <InfoTooltip text={question.info} />
+      <textarea
+        value={text}
+        onChange={handleChange}
+        placeholder="Type your answer here..."
+        maxLength={250}
+      />
+      <div>{text.length}/250</div>
+    </div>
+  );
+};
 
-export {TextInput}
+export  {TextInput};
--- src/components/Reusable/index.js
+++ src/components/Reusable/index.js
@@ -1,8 +1,8 @@
 import { BalanceRating } from "./BalanceRating";
 import { DateInput } from "./DateInput";
 import { DropdownQuestion } from "./DropdownQuestion";
-import { InfoTooltip } from "./InfoTooltip";
+
 import { QuestionWrapper } from "./QuestionWrapper";
 import { RatingQuestion } from "./RatingQuestion";
 import { TextInput } from "./TextInput";
-export { BalanceRating, DateInput, DropdownQuestion, InfoTooltip, QuestionWrapper, RatingQuestion, TextInput }
+export { BalanceRating, DateInput, DropdownQuestion, QuestionWrapper, RatingQuestion, TextInput }
