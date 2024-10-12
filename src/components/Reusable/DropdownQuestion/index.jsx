99ccbf95faa85cca319009eadcced77da9412431
unknown <d4n.kh4n@gmail.com>
2024-10-12 15:42:11 +0500

    drop down ui setting

--- src/components/Reusable/DropdownQuestion/index.jsx
+++ src/components/Reusable/DropdownQuestion/index.jsx
@@ -1,24 +1,34 @@
-// src/components/DropdownQuestion.js
+
 import React from 'react';
+import "./style.css";
 
 const DropdownQuestion = ({ question, onChange }) => {
+  console.log(question)
   const handleChange = (e) => {
     onChange(question.id, e.target.value);
   };
 
   return (
-    <div className="question-wrapper">
-      <label>{question.title}</label>
-      <select onChange={handleChange}>
-        <option value="">Select...</option>
-        {question.options.map((option, index) => (
-          <option key={index} value={option}>
-            {option}
-          </option>
-        ))}
-      </select>
+    <div className="container">
+      <label>
+        <p>1</p>
+        {question.title}
+      </label>
+      <div className="container_dropdown_wrapper">
+        <select className="container_dropdown" onChange={handleChange}>
+          <option value="">Select...</option>
+          {question.options.map((option, index) => (
+            <option key={index} value={option}>
+              {option}
+            </option>
+          ))}
+        </select>
+        <div className="line"></div> {/* This is the line next to the arrow */}
+      </div>
+
+
     </div>
   );
 };
 
-export {DropdownQuestion};
+export { DropdownQuestion };
