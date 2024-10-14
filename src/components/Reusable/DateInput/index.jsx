6d3ac51482538b1213de2c1b53ec0be94d3ebd1d
unknown <d4n.kh4n@gmail.com>
2024-10-14 10:21:45 +0500

    .

--- src/components/Reusable/DateInput/index.jsx
+++ src/components/Reusable/DateInput/index.jsx
@@ -16,30 +16,49 @@ const DateInput = ({ question, onChange }) => {
         <InfoTooltip text={question.info} />
       </div>
       <div className="date-inputs">
-        <input
-          type="number"
-          placeholder="DD"
-          min="1"
-          max="31"
-          value={date.day}
-          onChange={(e) => handleChange('day', e.target.value)}
-        />
-        <input
-          type="number"
-          placeholder="MM"
-          min="1"
-          max="12"
-          value={date.month}
-          onChange={(e) => handleChange('month', e.target.value)}
-        />
-        <input
-          type="number"
-          placeholder="YYYY"
-          min="1920"
-          max="2006"
-          value={date.year}
-          onChange={(e) => handleChange('year', e.target.value)}
-        />
+        <div className="input-container">
+          <label htmlFor="day">Day</label>
+          <input
+            id="day"
+            className="date-input"
+            type="number"
+            placeholder="DD"
+            min="1"
+            max="31"
+            value={date.day}
+            onChange={(e) => handleChange('day', e.target.value)}
+          />
+        </div>
+        <div className="input-container">
+          <label htmlFor="month">Month</label>
+          <input
+            id="month"
+            className="date-input"
+            type="number"
+            placeholder="MM"
+            min="1"
+            max="12"
+            value={date.month}
+            onChange={(e) => handleChange('month', e.target.value)}
+          />
+        </div>
+        <div className="input-container">
+          <label htmlFor="year">Year</label>
+          <input
+            id="year"
+            className="date-input"
+            type="number"
+            placeholder="YYYY"
+            min="1920"
+            max="2006"
+            value={date.year}
+            onChange={(e) => handleChange('year', e.target.value)}
+          />
+        </div>
+      </div>
+      <div className='button-container'>
+        <button className="ok-button" onClick={() => console.log("OK clicked")}>OK</button>
+        <span className="press-enter-text">or press Enter</span>
       </div>
     </div>
   )
