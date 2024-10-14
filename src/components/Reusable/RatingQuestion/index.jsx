7af2e73ae8a65de173c4c0e892cd2f52498e5267
Daniyal Khan <d4n.kh4n@gmail.com>
2024-10-14 13:04:18 +0500

    text input ui fixed

--- src/components/Reusable/RatingQuestion/index.jsx
+++ src/components/Reusable/RatingQuestion/index.jsx
@@ -1,21 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
+import "./style.css"
 
 const RatingQuestion = ({ question, onChange }) => {
+  const [isActive, setActive] = useState()
+  const handleGetRatting = (id, ratting) => {
+    console.log(id, ratting)
+    onChange(id, ratting)
+
+  }
   return (
-    <div className="question-wrapper">
-      <label>{question.title}</label>
+    <div className="container">
+      <label>
+        <p>3.1</p>
+        {question.title}
+      </label>
+      <label>
+        <p>3.2</p>
+        {question.subtitle}
+      </label>
       <div className="rating-buttons">
         {Array.from({ length: 10 }, (_, index) => index + 1).map((rating) => (
           <button
+            className='buttons-container'
             key={rating}
-            onClick={() => onChange(question.id, rating)}
+            onClick={() => handleGetRatting(question.id, rating)}
           >
             {rating}
           </button>
         ))}
       </div>
+      <div className='button-container'>
+        <button className="ok-button" onClick={() => console.log("OK clicked")}>OK</button>
+        <span className="press-enter-text">or press Enter</span>
+      </div>
     </div>
   );
 };
 
-export {RatingQuestion};
+export { RatingQuestion };
--- src/components/Reusable/TextInput/index.jsx
+++ src/components/Reusable/TextInput/index.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import InfoTooltip from '../InfoTooltip';
+import "./style.css"
 
 const TextInput = ({ question, onChange }) => {
   const [text, setText] = useState('');
@@ -13,18 +14,25 @@ const TextInput = ({ question, onChange }) => {
   };
 
   return (
-    <div className="question-wrapper">
-      <label>{question.title}</label>
-      <InfoTooltip text={question.info} />
+    <div className="container">
+      <div>
+        <label><p>4</p>{question.title}</label>
+        <InfoTooltip text={question.info} />
+      </div>
       <textarea
         value={text}
+        className='text-input'
         onChange={handleChange}
         placeholder="Type your answer here..."
         maxLength={250}
       />
       <div>{text.length}/250</div>
+      <div className='button-container'>
+        <button className="ok-button" onClick={() => console.log("OK clicked")}>OK</button>
+        <span className="press-enter-text">or press Enter</span>
+      </div>
     </div>
   );
 };
 
-export  {TextInput};
+export { TextInput };
